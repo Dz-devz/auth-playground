3d4a70c9ba415d55062dbcdc21d187368c4516d1
Dz-devz <darwinbjordan@gmail.com>
2024-06-19 20:57:59 +0800

    progress: added logout button and logout function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { logout } from "./logout/action";
+
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-center text-6xl">Auth Playground</h1>
+      <form action={logout}>
+      <button type="submit">
+        Logout
+      </button>
+      </form>
     </main>
   );
 }
