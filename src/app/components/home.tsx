2477e15674d4187056a7e14a63dfa6ef7d7eea14
Dz-devz <darwinbjordan@gmail.com>
2024-07-04 20:33:31 +0800

    chore: ui update on home and login page

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -2,7 +2,6 @@
 
 import { createClient } from '@/utils/supabase/client';
 import { useEffect, useState } from 'react';
-import Logout from './logout';
 
 interface User {
     id: string;
@@ -31,7 +30,9 @@ export default function  Home() {
       <h1 className='text-center text-6xl'>
       Welcome to my Auth Playground!
       </h1>
-      <p className='w-[1100px] mt-5 text-2xl text-center'>Here, you&apos;re diving into the world of authentication, where every challenge and solution is a step toward mastering the art of securing access and verifying identities. This playground is your dedicated space for hands-on learning, where concepts like Supabase Auth and session management come to life through experimentation and practice. Whether you're exploring different authentication methods, refining your understanding of security protocols, or fine-tuning your skills in implementing authentication mechanisms, this playground is where theory meets practical application. It's a place where mistakes are learning opportunities and breakthroughs are celebrated milestones on your journey to becoming proficient in authentication. So, dive in, experiment fearlessly, and let your auth playground be the forge where your expertise in authentication is shaped and honed.</p>
+      <p className='w-[1100px] mt-5 text-2xl text-center'>
+        Here, you&apos;re diving into the world of authentication, where every challenge and solution is a step toward mastering the art of securing access and verifying identities. This playground is your dedicated space for hands-on learning, where concepts like Supabase Auth and session management come to life through experimentation and practice. Whether you&apos;re exploring different authentication methods, refining your understanding of security protocols, or fine-tuning your skills in implementing authentication mechanisms, this playground is where theory meets practical application. It&apos;s a place where mistakes are learning opportunities and breakthroughs are celebrated milestones on your journey to becoming proficient in authentication. So, dive in, experiment fearlessly, and let your auth playground be the forge where your expertise in authentication is shaped and honed.
+      </p>
     </div>
   )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,17 +8,22 @@ export default function LoginPage() {
   // ...
 
   return (
+    <div className="flex items-center justify-center flex-col gap-4 mt-20">
+      <h1 className="text-center text-2xl">Wanna know the secret? <br/> Log in now!</h1>
       <form>
         <Label htmlFor="email">Email</Label>
         <Input id="email" name="email" placeholder="Email" />
         <Label htmlFor="password">Password</Label>
         <Input id="password" name="password" placeholder="Password" />
+        <div className="mt-2 flex gap-4 items-center justify-center">
         <Button type="submit">
           <button formAction={login}>Log In</button>
         </Button>
-        <Button className="mr-2 ml-2" type="submit">
+        <Button type="submit">
           <button formAction={signUp}>Sign un</button>
         </Button>
+        </div>
       </form>
+    </div>
   )
 }
