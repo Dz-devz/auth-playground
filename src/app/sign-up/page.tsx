addda4845281784302b5fe5e386326a871edc013
Dz-devz <darwinbjordan@gmail.com>
2024-07-10 19:52:16 +0800

    chore: change formAction to action

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -12,13 +12,13 @@ export default function SignInPage() {
   return (
     <div className="flex items-center justify-center flex-col gap-4 mt-20">
       <h1 className="text-center text-2xl">Wanna know the secret? <br/> Log in now!</h1>
-      <form>
+      <form action={signUpNewUser}>
         <Label htmlFor="email">Email</Label>
         <Input id="email" name="email" placeholder="Email" type="email" />
         <Label htmlFor="password">Password</Label>
         <Input id="password" name="password" placeholder="Password" type="password" />
         <div className="mt-2 flex gap-4 items-center justify-center">
-        <Button type="submit" className="disabled:opacity-70" disabled={pending} formAction={signUpNewUser}>
+        <Button type="submit" className="disabled:opacity-70" disabled={pending}>
         {pending ? ( <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white mr-auto ml-auto"/> )
         : (
           <>Sign Up</>
