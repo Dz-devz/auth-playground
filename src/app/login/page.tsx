db08dc0d64c658f06491ccd09866f1980f6e8f18
Dz-devz <darwinbjordan@gmail.com>
2024-07-15 19:28:44 +0800

    chore: update import and change formAction to Action in form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
+import { login } from "@/action/login"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { login } from "./action"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
 
@@ -11,13 +11,13 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center flex-col gap-4 mt-20">
       <h1 className="text-center text-2xl">Wanna know the secret? <br/> Log in now!</h1>
-      <form>
+      <form action={login}>
         <Label htmlFor="email">Email</Label>
         <Input id="email" name="email" placeholder="Email" type="email" />
         <Label htmlFor="password">Password</Label>
         <Input id="password" name="password" placeholder="Password" type="password" />
         <div className="mt-2 flex gap-4 items-center justify-center">
-        <Button type="submit" formAction={login}>
+        <Button type="submit">
           Log in
         </Button>
         <Button>
